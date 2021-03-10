6d53ea34fd9ff64b82bab8ee6a54cca3c0f9a767
Willow Bannister <79249427+WillowBannister@users.noreply.github.com>
2021-03-10 14:21:10 +0000

    Update run - Fix

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Willow!'
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
